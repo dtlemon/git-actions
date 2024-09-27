be179fe490454c8a5ecaa4c7492dd620e05c2825
Dylan <dtlemon@anderson.edu>
2024-09-27 10:56:35 -0400

    Add calc page and update tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 from webapp.app import app
 import pytest
+from datetime import datetime
 
 @pytest.fixture()
 def client():
@@ -7,5 +8,12 @@ def client():
 
 def test_root(client):
     response = client.get("/")
+    date_raw = datetime.now()
+    date_pretty = date_raw.strftime("%m-%d-%Y")
     print(response.data)
-    assert b"Hello" in response.data
+    assert date_pretty.encode() in response.data
+
+def test_calc(client):
+    response = client.get("/calc?a=5&op=*&b=3")
+    print(response.data)
+    assert b"15" in response.data
--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,60 @@
-from flask import Flask
+from flask import Flask, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    page = """"""
+    date_raw = datetime.now()
+    date_pretty = date_raw.strftime("%m-%d-%Y")
+    page += f"{date_pretty}"
+    page += """
+    <form action=/>
+        <h1>Links:</h1>
+    </form>
+
+    
+    """
+    return page
+
+@app.route('/calc')
+def calc():
+    # a = int(request.args["a"])
+    # b = int(request.args["b"])
+# 
+    # return f"{a + b}"
+    page = """
+    <form action=/calc method=get>
+        <input name=a />
+        <select name="op" id="operator">
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="*">*</option>
+          <option value="/">/</option>
+        </select>
+        <input name=b />
+        <input type=submit />
+    </form>
+    """
+
+    try:
+        a = int(request.args["a"])
+        op = request.args["op"]
+        b = int(request.args["b"])
+
+        if (op == "+"):
+            page += f"<p>Answer: {a+b}</p>"
+        elif (op == "-"):
+            page += f"<p>Answer: {a-b}</p>"
+        elif (op == "*"):
+            page += f"<p>Answer: {a*b}</p>"
+        elif (op == "/"):
+            page += f"<p>Answer: {a/b}</p>"
+        else:
+            pass
+
+    except:
+        pass
+
+    return page
